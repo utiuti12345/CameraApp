20d8f1fc607b3868b5e411b3ac2340cd1952887e
Yuta Saotome <private.uta.sao@gmail.com>
2020-04-15 19:03:06 +0900

    Create CameraApp for SwiftStudy

---
+++ CameraApp/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by saotome on 2020/04/15.
+//  Copyright © 2020 saotome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    @IBAction func cameraLaunchAction(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            print("Camera can be used.")
+            let ipc = UIImagePickerController()
+            ipc.sourceType = .camera
+            ipc.delegate = self
+            present(ipc,animated: true,completion: nil)
+        } else {
+            print("Camera is not avaiable.")
+        }
+    }
+    @IBAction func shareAction(_ sender: Any) {
+        if let sharedImage = photoImage.image {
+            let sharedItems = [sharedImage]
+            let controller = UIActivityViewController(activityItems: sharedItems, applicationActivities: nil)
+            controller.popoverPresentationController?.sourceView = view
+            present(controller,animated: true,completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        photoImage.image = info[.originalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+    }
+}
+
